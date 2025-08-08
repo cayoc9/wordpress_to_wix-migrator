c626d147cc3f7e78e473e93ea5295c4c659e5257
Cayo Murilo <116114268+cayoc9@users.noreply.github.com>
2025-08-07 22:54:07 -0700

    Add extractors package initializer

---
+++ src/extractors/__init__.py
@@ -0,0 +1,9 @@
+"""
+Extractors for WordPress export files.
+
+This subpackage provides functions to parse CSV and XML exports from
+WordPress into a normalized internal representation used by the Wix
+migrator.  The normalized post dictionaries have consistent keys,
+making it easier to convert them to Rich Content and to call the Wix
+APIs.
+"""
